a1f9bab5a83381d3baa3911b0b06cdb6f53ba1ce
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-18 06:07:37 +0000

    Adding tarfile member sanitization to extractall()

--- src/octoprint/server/api/languages.py
+++ src/octoprint/server/api/languages.py
@@ -197,7 +197,26 @@ def _unpack_uploaded_tarball(path, target):
         map(_validate_archive_name, tar.getmembers())
 
         # unpack everything
-        tar.extractall(target)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, target)
 
 
 def _validate_archive_name(name):
